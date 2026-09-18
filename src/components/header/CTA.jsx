63e7e73c7535d1d642ebdf51248b118a6d435b1a
Abbas <98712750+abiasV@users.noreply.github.com>
2026-09-18 11:43:02 -0400

    Improve hero calls to action

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import CV from "../../assets/AbbasVaziri-Resume.pdf"
+import React from "react";
+import CV from "../../assets/AbbasVaziri-Resume.pdf";
 
 const CTA = () => {
   return (
-    <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    <div className="cta">
+      <a href="#portfolio" className="btn btn-primary">
+        View Projects
+      </a>
+      <a href={CV} download className="btn">
+        Download Resume
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
